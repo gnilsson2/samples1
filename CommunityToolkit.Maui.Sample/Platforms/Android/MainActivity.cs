6a8d9221eecd2c571bea24f0c362ea29f349884e
gnilsson2 <gnilsson2@msn.com>
2025-01-23 23:24:43 +0100

    Tranparent Splash

--- CommunityToolkit.Maui.Sample/Platforms/Android/MainActivity.cs
+++ CommunityToolkit.Maui.Sample/Platforms/Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Microsoft.Maui;
 
 namespace CommunityToolkit.Maui.Sample.Platforms.Android;
 
-[Activity(Theme = "@style/Maui.SplashTheme", ResizeableActivity = true, MainLauncher = true, LaunchMode = LaunchMode.SingleTask, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+[Activity(Theme = "@style/MainTheme", ResizeableActivity = true, MainLauncher = true, LaunchMode = LaunchMode.SingleTask, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
 public class MainActivity : MauiAppCompatActivity
 {
 
